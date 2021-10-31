cbb56907addfd51ab680a09f078ec184b44141f1
dm-marina <mysummer1404@gmail.com>
2021-10-31 15:08:38 +0200

    dm-marina

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+for(let num=+prompt('Enter number:');num>0;){
+    if(num%2!=0 && num/1===num && num/num===1){
+        // num === 'true'; 
+        console.log('true');
+        break    
+    } else{
+        // num === 'false';
+        console.log('false');
+        break
+    }
+    
+}
